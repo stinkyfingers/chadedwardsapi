d392dc64b981c7599ece11e174f381b70e1b7be6
johnshenk <jshenk@demandanlytics.io>
2023-06-12 13:17:57 -0500

    increase lambda timeout

--- terraform/main.tf
+++ terraform/main.tf
@@ -86,6 +86,7 @@ resource "aws_lambda_function" "server" {
   handler          = "lambda-lambda"
   runtime          = "go1.x"
   source_code_hash = filebase64sha256("../lambda.zip")
+  timeout          = 15
   environment {
     variables = {
       TWILIO_USER        = data.aws_ssm_parameter.twilio_user.value
